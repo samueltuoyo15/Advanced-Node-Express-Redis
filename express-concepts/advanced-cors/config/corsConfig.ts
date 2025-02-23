4e109abe0d8a5f95cb98723a584eeca17bc82d37
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-23 19:34:02 +0100

    express concepts

--- express-concepts/advanced-cors/config/corsConfig.ts
+++ express-concepts/advanced-cors/config/corsConfig.ts
@@ -4,6 +4,12 @@ const corsConfig = () => {
   return cors({
     origin: (origin, callback) => {
       const allowedOrigins = ["http://localhost:3000"]
+      
+      if(!origin || allowedOrigins.indexOf(origin) !== -1){
+        callback(null, true)
+      } else{
+        callback(new Error("Sorry your origin is whitelisted from using our api"))
+      }
     }
   })
 }
